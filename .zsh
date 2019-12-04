4d71ec155f997935ca52adc9250392dea904274d
theocikos <49213748+theocikos@users.noreply.github.com>
2019-12-04 15:39:43 +0200

    Create .zsh

---
+++ .zsh
@@ -0,0 +1,17 @@
+#Update or create .zsh file for aliases
+
+#Navigation
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+
+# Shortcuts
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias g="git"
+alias h="history"
+
